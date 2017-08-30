946f5d0facd820ba375d2674ba719dd8ecf09b69
Lukasz <manix93@gmail.com>
2017-08-30 12:06:23 +0200

    Add associacion between models, create tables and add seeds

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,2 +1,4 @@
 class Part < ApplicationRecord
+  belongs_to :movies
+  belongs_to :actor
 end
--- db/migrate/20170830085830_create_parts.rb
+++ db/migrate/20170830085830_create_parts.rb
@@ -1,6 +1,8 @@
 class CreateParts < ActiveRecord::Migration[5.0]
   def change
     create_table :parts do |t|
+      t.belongs_to :movie, index: true, optional: true
+      t.belongs_to :actor, index: true, optional: true
 
       t.timestamps
     end
