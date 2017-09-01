5a0ec0bad38c885c0fffc0cadf6676a16f0c5636
Lukasz <manix93@gmail.com>
2017-09-01 14:19:07 +0200

    generate user model with has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830085830) do
+ActiveRecord::Schema.define(version: 20170901121450) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "first_name"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20170830085830) do
     t.datetime "updated_at", null: false
     t.index ["actor_id"], name: "index_parts_on_actor_id"
     t.index ["movie_id"], name: "index_parts_on_movie_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
