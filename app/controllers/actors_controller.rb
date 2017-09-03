f98a69cbf1852d4e09d5a6ea15405b899ee6bdbe
Lukasz <manix93@gmail.com>
2017-09-03 12:11:27 +0200

    Add authenticate with logout

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,6 @@
 class ActorsController < ApplicationController
+  before_action :require_user, only: [:index, :show]
+
   def index
     @actors = Actor.all
   end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :require_user, only: [:index, :show]
+
   def index
     @movies = Movie.all
   end
